25817c6c31e228242f40108d601ba29277d63eca
cayman444 <artursavinX@yandex.ru>
2026-07-24 11:58:06 +0300

    align product details layout by adding items-start and self-start classes

--- frontend/src/entities/product/ui/ProductDetails.vue
+++ frontend/src/entities/product/ui/ProductDetails.vue
@@ -19,12 +19,10 @@ const emit = defineEmits<{
 <template>
   <ProductDetailsSkeleton v-if="isLoading" />
   <ProductDetailsError v-else-if="error" :error="error" />
-  <div v-else-if="product" class="grid grid-cols-1 lg:grid-cols-12 gap-12 pb-24">
+  <div v-else-if="product" class="grid grid-cols-1 lg:grid-cols-12 gap-12 pb-24 items-start">
     <ProductDetailsCarousel :product />
-    <div class="pt-4 lg:col-span-5 lg:sticky lg:top-32 h-fit">
+    <div class="pt-4 lg:col-span-5 lg:sticky lg:top-32 h-fit self-start">
       <ProductDetailsInfo :product @add-to-cart="emit('addToCart')" />
     </div>
   </div>
 </template>
-
-
